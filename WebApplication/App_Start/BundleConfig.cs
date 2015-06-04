a5a4279b94fd6d0004f5599cd3d4ca003353fc8e
NielsHoiting <niels.hoiting@nubisonline.nl>
2015-06-04 11:34:28 +0200

    Added Offcanvas

--- WebApplication/App_Start/BundleConfig.cs
+++ WebApplication/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace WebApplication
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/offcanvas").Include(
+                        "~/Scripts/offcanvas.js"));
         }
     }
 }
