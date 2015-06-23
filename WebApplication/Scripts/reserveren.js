8a709395ccb97449b4eb7910190011147a5b1865
NielsHoiting <niels.hoiting@nubisonline.nl>
2015-06-23 23:52:06 +0200

    Added modal for reserveren

---
+++ WebApplication/Scripts/reserveren.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $('.btn-inschrijven').click(function () {
+        var lesid = $(this).attr('id');
+        $.post('/Reserveren/GetLesData', { id: lesid }, function (data) {
+            var les = $.parseJSON(data);
+            $('#myModal').modal('show');
+        });
+    });   
+});
