6e0e64b83aaa9d56ca1b8a0ea4c73ee2c02f4576
markvdhorst <markvdhorst0@gmail.com>
2015-06-26 01:58:28 +0200

    Login Checks

--- WebApplication/Persistance/AccountPersistenceManager.cs
+++ WebApplication/Persistance/AccountPersistenceManager.cs
@@ -17,7 +17,9 @@ namespace WebApplication.Persistance
             using (ISession session = OpenSession())
             {
                 ICriteria criteria = session.CreateCriteria(typeof(Gebruiker));
-                criteria.Add(Expression.Eq("sco_nummer", Int32.Parse(username)));
+                int sco_nummer = 0;
+                Int32.TryParse(username, out sco_nummer);
+                criteria.Add(Expression.Eq("sco_nummer", sco_nummer));
                 criteria.Add(Expression.Eq("wachtwoord", password));
                 IList<Gebruiker> matchingObjects = criteria.List<Gebruiker>();
                 if (matchingObjects.Count() > 0)
